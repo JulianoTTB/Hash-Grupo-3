e8594c919b1bc37b279681ff90083ce9dc1d5f5a
GeandroMarques <marquess.2608@gmail.com>
2025-10-15 20:53:08 -0300

    metodo para ver as 3 maiores listas encadeadas

--- codigoComentado/HashMultiplicacao.java
+++ codigoComentado/HashMultiplicacao.java
@@ -31,7 +31,7 @@ public class HashMultiplicacao {
         if (tabelaHash[hash] == null) {
             tabelaHash[hash] = new ListaEncadeada();
         }
-        // Caso a lista já exista, insere o registro e o hash na posição.
+        // Caso a lista já exista, insere o registro e o hash na posição e retorna o número de colisões.
         return tabelaHash[hash].inserir(registro);
     }
 
@@ -50,8 +50,10 @@ public class HashMultiplicacao {
 
         // Verifica se existe algum valor armazenado na posição.
         if (tabelaHash[hash] != null) {
+            // Retorna o número de colisões.
              return tabelaHash[hash].buscar(codigo);
         }
+        // Caso não exista na tabela hash, retorna -1.
         return -1;
     }
 
@@ -65,25 +67,32 @@ public class HashMultiplicacao {
             if(tabelaHash[i] != null) {
                 int tamanho = tabelaHash[i].getTamanho();
 
-                // Compara o tamanho da posição t
+                // Compara o tamanho da lista na posiçao i da tabela hash com a maior lista do vetor maioresListas.
                 if(tamanho > maioresListas[0]) {
-                    maioresListas[2] = maioresListas[1];
-                    indiceMaiores[2] = indiceMaiores[1];
+                    // Verdadeiro caso o tamanho da lista encadeada i na tabela hash seja maior que a maior lista do vetor listaMaiores.
+                    maioresListas[2] = maioresListas[1]; // Coloca a segunda maior lista na terceira posição.
+                    indiceMaiores[2] = indiceMaiores[1]; // Altera o indice da segunda maior lista para a terceira posição.
 
-                    maioresListas[1] = maioresListas[0];
-                    indiceMaiores[1] = indiceMaiores[0];
+                    maioresListas[1] = maioresListas[0]; // Coloca a maior lista na segunda posição.
+                    indiceMaiores[1] = indiceMaiores[0]; // Altera o indice da maior lista para a segunda posição.
 
-                    maioresListas[0] = tamanho;
-                    indiceMaiores[0] = i;
+                    maioresListas[0] = tamanho; // Insere o tamanho da lista encadeada i da tabela hash na primeiro posição do vetor.
+                    indiceMaiores[0] = i; // Insere o índice da posição da lista encadeada na primeira primeira posição do vetor.
+
+                // Verifica o tamanho da lista na posiçao i da tabela hash com a segunda maior lista do vetor maioresListas.
                 } else if(tamanho > maioresListas[1]) {
-                    maioresListas[2] = maioresListas[1];
-                    indiceMaiores[2] = indiceMaiores[1];
 
-                    maioresListas[1] = tamanho;
-                    indiceMaiores[1] = i;
+                    maioresListas[2] = maioresListas[1]; // Coloca a segunda maior lista na terceira posição.
+                    indiceMaiores[2] = indiceMaiores[1]; // Altera o indice da segunda maior lista para a terceira posição.
+
+                    maioresListas[1] = tamanho; // Insere o tamanho da lista encadeada i da tabela hash na segunda posição do vetor.
+                    indiceMaiores[1] = i; // Insere o índice da posição da lista encadeada na segunda posição do vetor.
+
+                    // Verifica o tamanho da lista na posiçao i da tabela hash com a terceira maior lista do vetor maioresListas.
                 } else if(tamanho > maioresListas[2]) {
-                    maioresListas[2] = tamanho;
-                    indiceMaiores[2] = i;
+
+                    maioresListas[2] = tamanho; // Insere o tamanho da lista encadeada i da tabela hash na terceira posição do vetor maioresListas.
+                    indiceMaiores[2] = i; // Insere o indice da posição da lista encadeada na terceira posiçao do vetor.
                 }
             }
         }
